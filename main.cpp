0485d37284b05806f7a525cfe63dafde30dd6cff
luchk <30504944+luchk@users.noreply.github.com>
2017-11-18 13:50:09 +0200

    start

--- main.cpp
+++ main.cpp
@@ -1,6 +1,50 @@
 #include <iostream>
+#define SIZE 5
+
+using namespace std;
+
+void fill(int massive[]){
+    for(int i=0;i<SIZE;i++){
+        cout<<"["<<i+1<<"]:";
+        cin>> massive[i];
+    }
+}
+
+void fill_out(int massive[]){
+    for(int i=0;i<SIZE;i++){
+        cout<<massive[i]<<" ";
+    }
+}
+
+void bubbleSort(int massive[]){
+    for(int i=0;i<SIZE-1;i++){
+        for(int j=0;j<SIZE;j++){
+            if(massive[j]>massive[j+1])
+                swap(massive[j],massive[j+1]);
+
+        }
+    }
+}
+
+void output(int massive[]){
+    for(int i=0;i<SIZE;i++){
+        cout<<massive[i]<<" ";
+    }
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    int n,massive[SIZE];
+
+    cout<<"Fill the massive:"<<endl;
+    fill(massive);
+    cout<<"Our massive is: ";
+    fill_out(massive);
+    cout<<"\nThe sorted massive is: ";
+    bubbleSort(massive);
+    output(massive);
+
+
+
     return 0;
 }
